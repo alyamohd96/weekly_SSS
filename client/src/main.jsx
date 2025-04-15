7417c1a92755c946889019af378bd03e48eace4f
Alya <alyamohd2102@gmail.com>
2025-04-15 08:29:52 +0800

    Created Home, Login, and Register pages and Navbar component

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
