da3ebdc18cdf4a830b7318fd3df013b0f2095f5c
Alya <alyamohd2102@gmail.com>
2025-04-15 08:35:24 +0800

    remove 'React.' in main.jsx to make it work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Navbar from '../src/components/Navbar'
-import Home from '../src/pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from '../src/components/Navbar';
+import Home from '../src/pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
 
