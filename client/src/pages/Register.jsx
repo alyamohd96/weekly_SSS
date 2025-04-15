506986fd66cf52f8a56f76ad862706d7e6a7c3ea
Alya <alyamohd2102@gmail.com>
2025-04-15 08:41:08 +0800

    add simple name, email, and password in Register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export default function Register() {
-  return (
-    <div>
-      Register
-    </div>
-  )
+
+    const registerUser = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <form onSubmit={registerUser}>
+                <label>Name</label>
+                <input type='text' placeholder='enter name...'/>
+                <label>Email</label>
+                <input type='text' placeholder='enter email...'/>
+                <label>Password</label>
+                <input type='password' placeholder='enter password...'/>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
 }
