c873ec38b0c6401ddd07832c03acbc45124b55ca
Alya <alyamohd2102@gmail.com>
2025-04-17 22:07:35 +0800

    send the hashedPassword to the database instead of just the password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const { hashPassword, comparePassword } = require('../helpers/auth')
 
 // defining the test function that will be called from authRoutes
 const test = (req, res) => {
@@ -29,9 +30,11 @@ const registerUser = async(req, res) => {
             })
         }
 
+
+        const hashedPassword = await hashPassword(password)
         // create user in database
         const user = await User.create({
-            name, email, password
+            name, email, password: hashedPassword
         })
 
         return res.json(user)
